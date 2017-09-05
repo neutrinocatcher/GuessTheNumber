8c50df9a69b509495e7901fb7d404e3c856aa13a
Dimitrijs Fedotovs <dima@fedoto.ws>
2017-09-05 23:09:21 +0300

    formatting and minor fixes

--- src/guru/bug/guess/Main.java
+++ src/guru/bug/guess/Main.java
@@ -8,73 +8,120 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
-    private static final Path leaderboardFilePath = Paths.get("leaderboard.txt");
-
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Path boardPath = Paths.get("leaderboard.txt");
+    private static final List<GameResult> leaderBoard = loadLeaderBoard();
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        List<GameResult> resultList = loadLeaderboard();
         do {
-            GameResult result = new GameResult();
-            System.out.println("Enter your name");
-            result.name = scanner.next();
+            GameResult gameResult = new GameResult();
+            gameResult.name = askString("Enter your name: ", 3, 100);
+            long t1 = System.currentTimeMillis();
             int myNum = random.nextInt(100) + 1;
             System.out.println("I'm thinking a number from 1 to 100. Try to guess it!");
             boolean userWin = false;
             for (int attempt = 1; attempt <= 10; attempt++) {
-                System.out.printf("Attempt #%d. Enter your guess: ", attempt);
-                int userNum = scanner.nextInt();
+                String msg = String.format("Attempt #%d. Enter your guess: ", attempt);
+                int userNum = askNumber(msg, 1, 100);
                 if (myNum > userNum) {
                     System.out.println("Your number is too low");
                 } else if (myNum < userNum) {
                     System.out.println("Your number is too high");
                 } else if (myNum == userNum) {
+                    long t2 = System.currentTimeMillis();
+                    gameResult.time = t2 - t1;
+                    gameResult.attempts = attempt;
+                    leaderBoard.add(gameResult);
+                    leaderBoard.sort(Comparator.<GameResult>comparingInt(r -> r.attempts)
+                            .thenComparingLong(r -> r.time));
                     System.out.printf("You won! %d attempts were used.\n", attempt);
                     userWin = true;
-                    result.attempts = attempt;
-                    resultList.add(result);
                     break;
                 }
             }
             if (!userWin) {
                 System.out.printf("You lost! My number was %d\n", myNum);
             }
-            resultList.sort(Comparator.<GameResult>comparingInt(r -> r.attempts).thenComparingLong(r -> r.time));
-            for (GameResult r : resultList) {
-                System.out.println(r.name + "  " + r.attempts);
-            }
+
+            printLeaderBoard();
+
             System.out.print("Do you want to play again? (Y/n) ");
-        } while (!scanner.next().equals("n"));
+        } while (!scanner.nextLine().equals("n"));
         System.out.println("Good bye!");
-        storeLeaderboard(resultList);
+        storeLeaderBoard();
     }
 
-    private static List<GameResult> loadLeaderboard() {
+    private static void printLeaderBoard() {
+        System.out.println("Leader Board:");
+        System.out.println("\t Name \t\t Attempts \t\t Time");
+        int maxDisplay = Math.min(4, leaderBoard.size());
+        List<GameResult> top = leaderBoard.subList(0, maxDisplay);
+        for (GameResult r : top) {
+            System.out.printf("\t %s \t\t %8d \t\t %5.1f sec\n", r.name, r.attempts, r.time / 1000.0);
+        }
+    }
+
+    private static String askString(String message, int min, int max) {
+        while (true) {
+            System.out.print(message);
+            String result = scanner.nextLine();
+            if (result.length() < min) {
+                System.out.printf("String should not be shorter than %d characters\n", min);
+            } else if (result.length() > max) {
+                System.out.printf("String should not be longer than %d character\n", max);
+            } else {
+                return result;
+            }
+        }
+    }
+
+    private static int askNumber(String message, int min, int max) {
+        while (true) {
+            System.out.print(message);
+            try {
+                int result = scanner.nextInt();
+                scanner.nextLine(); // clear everything what user entered after the number
+                if (result < min) {
+                    System.out.printf("Number should not be less than %d\n", min);
+                } else if (result > max) {
+                    System.out.printf("Number should not be greater than %d\n", max);
+
+                } else {
+                    return result;
+                }
+            } catch (InputMismatchException e) {
+                String str = scanner.nextLine();
+                System.out.printf("%s is not a number\n", str);
+            }
+        }
+    }
+
+    private static List<GameResult> loadLeaderBoard() {
         List<GameResult> list = new ArrayList<>();
-        try (Scanner lbscanner = new Scanner(leaderboardFilePath)) {
-            while (lbscanner.hasNext()) {
+        try (Scanner in = new Scanner(boardPath)) {
+            while (in.hasNext()) {
                 GameResult result = new GameResult();
-                result.name = lbscanner.next();
-                result.attempts = lbscanner.nextInt();
-                result.time = lbscanner.nextLong();
+                result.name = in.next();
+                result.attempts = in.nextInt();
+                result.time = in.nextLong();
                 list.add(result);
             }
         } catch (IOException e) {
-            System.out.println("cannot read leader board");
+            System.out.println("Sorry, cannot read leader board");
         }
         return list;
     }
 
-    private static void storeLeaderboard(List<GameResult> resultList) {
-        try (Writer out = Files.newBufferedWriter(leaderboardFilePath)) {
-            for (GameResult r : resultList) {
-                String line = String.format("%s %d %d", r.name, r.attempts, r.time);
+    private static void storeLeaderBoard() {
+        try (Writer out = Files.newBufferedWriter(boardPath)) {
+            for (GameResult r : leaderBoard) {
+                String line = String.format("%s %d %d\n", r.name, r.attempts, r.time);
                 out.write(line);
-                out.write("\n");
             }
         } catch (IOException e) {
             System.out.println("Sorry, cannot store leader board");
         }
     }
+
 }
